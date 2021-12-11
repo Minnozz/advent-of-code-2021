83541a5023f714bf4f43d64f8a8ebbb04ea8ff06
Bart Schuurmans <bart@minnozz.com>
2021-12-11 17:04:32 +0100

    Improve day 1

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,7 @@ slidingWindow size list =
     transpose $ [subList i | i <- [0..size-1]]
   where
     subList i = (take len . drop i) list
-    len = length list - size
+    len = length list - size + 1
 
 day1part1 :: [Int] -> Int
 day1part1 depths =
@@ -18,10 +18,10 @@ day1part1 depths =
 
 day1part2 :: [Int] -> Int
 day1part2 depths =
-    length $ filter id increases
+    length $ filter (uncurry (>)) pairs
   where
     sums = sum <$> slidingWindow 3 depths
-    increases = zipWith (>) (tail sums) sums
+    pairs = zip (tail sums) sums
 
 main :: IO ()
 main = do
