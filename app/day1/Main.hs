cdb2d117c6489a735afb4f9ec918bb75afda854f
Bart Schuurmans <bart@minnozz.com>
2021-12-11 17:08:55 +0100

    day1 executable

--- app/day1/Main.hs
+++ app/day1/Main.hs
@@ -9,14 +9,14 @@ slidingWindow size list =
     subList i = (take len . drop i) list
     len = length list - size + 1
 
-day1part1 :: [Int] -> Int
-day1part1 depths =
+part1 :: [Int] -> Int
+part1 depths =
     length $ filter id increases
   where
     increases = zipWith (>) (tail depths) depths
 
-day1part2 :: [Int] -> Int
-day1part2 depths =
+part2 :: [Int] -> Int
+part2 depths =
     length $ filter (uncurry (>)) pairs
   where
     sums = sum <$> slidingWindow 3 depths
@@ -26,6 +26,6 @@ main :: IO ()
 main = do
     bytes <- readFile "res/day1/input.txt"
     let depths = read <$> lines bytes
-    print $ day1part1 depths
-    print $ day1part2 depths
+    print $ part1 depths
+    print $ part2 depths
     pure ()
