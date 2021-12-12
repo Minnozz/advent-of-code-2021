0a86c864feffca0fcd0421291ab486eec454edc1
Bart Schuurmans <bart@minnozz.com>
2021-12-12 13:53:33 +0100

    Improve day 1

--- app/day1/Main.hs
+++ app/day1/Main.hs
@@ -4,28 +4,22 @@ import Data.List ( transpose )
 
 slidingWindow :: Int -> [a] -> [[a]]
 slidingWindow size list =
-    transpose $ [subList i | i <- [0..size-1]]
+    take len $ transpose $ map subList [0..size-1]
   where
-    subList i = (take len . drop i) list
+    subList i =  drop i list
     len = length list - size + 1
 
-part1 :: [Int] -> Int
-part1 depths =
-    length $ filter id increases
-  where
-    increases = zipWith (>) (tail depths) depths
-
-part2 :: [Int] -> Int
-part2 depths =
+countIncreases :: Int -> [Int] -> Int
+countIncreases windowSize depths =
     length $ filter (uncurry (>)) pairs
   where
-    sums = sum <$> slidingWindow 3 depths
+    sums = sum <$> slidingWindow windowSize depths
     pairs = zip (tail sums) sums
 
 main :: IO ()
 main = do
     bytes <- readFile "res/day1/input.txt"
     let depths = read <$> lines bytes
-    print $ part1 depths
-    print $ part2 depths
+    print ("Part 1", countIncreases 1 depths)
+    print ("Part 2", countIncreases 3 depths)
     pure ()
